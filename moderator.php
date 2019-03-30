a37a5f0fbbd0e35b3137b3953981d02ea69f97b3
EMChadwick <47214282+EMChadwick@users.noreply.github.com>
2019-03-30 21:25:56 +0000

    Style work in progress
    Making the mod screen look pretty

--- moderator.php
+++ moderator.php
@@ -3,36 +3,43 @@
     $pagetitle = "Questions";
     include ("pageComponents/topBar.php");
 	$head = $_GET['sessionID'];
-	echo '<h1>Room Number: '.$head.'</h1>';
+	
+	$sessionID = $_GET['sessionID'];
+	$url = "https://cadgroup2.jdrcomputers.co.uk/api/questions/session/{$sessionID}";
+    $jsondata = file_get_contents($url);
+	$json = json_decode($jsondata, true);
+	
+	$nameURL = "https://cadgroup2.jdrcomputers.co.uk/api/sessions/{$sessionID}";	
+	$nameJsonData = file_get_contents($nameURL);
+	$nameJson = json_decode($nameJsonData, true);
+	//$roomTitle = json_decode(file_get_contents("https://cadgroup2.jdrcomputers.co.uk/api/sessions/{$sessionID}"), true)
+	echo '<h1 id="sessionName">'.$nameJson['sessionName'].'</h1>';
 ?>
-        <div class="list">
-          <?php
-	    $sessionID = $_GET['sessionID'];
-	    $url = "https://cadgroup2.jdrcomputers.co.uk/api/questions/session/{$sessionID}";
-        $jsondata = file_get_contents($url);
-	    $json = json_decode($jsondata, true);
-        $output = '<ul id="selections">';
-	    foreach($json as $question) {
-				$id = $question['userID'];
-				$users = "https://cadgroup2.jdrcomputers.co.uk/api/users/{$id}";
-				$userdata = file_get_contents($users);
-				$userjson = json_decode($userdata, true);
-                $output .= "<li>".$userjson['username'].": ".$question['question']."<div style='display: none;'>".'-'.$question['questionID']."</div></li>";
-            }
-            $output .= "</ul>";
-	    echo $output;
-	  ?>
-	</div>
+	<!--/Refresh Button and speaker button-->
+<div id="optionsButtons" style="padding-left: 10px; padding-bottom: 30px;">
+	<div  id="refreshButton"  class="btn btn-success useButton" onclick="return Refresh();" style="padding: 7px;">Refresh</div>
+	<div  id="speakerButton"  class="btn btn-success useButton" onclick="window.location.href = 'speaker.php'" style="padding: 7px;">Go to Speaker</div>
+</div>
+<div class="list">
+    <?php
+		$output = '<ul id="selections">';
+		foreach($json as $question) {
+			$id = $question['userID'];
+			$users = "https://cadgroup2.jdrcomputers.co.uk/api/users/{$id}";
+			$userdata = file_get_contents($users);
+			$userjson = json_decode($userdata, true);
+			$output .= "<li>".$userjson['username'].": ".$question['question']."<div style='display: none;'>".'-'.$question['questionID']."</div></li>";
+		}
+		$output .= "</ul>";
+		echo $output;
+	?>
+</div>
 
-	<div class="submissionblock">
-	<!--/Refresh Button-->
-	<button type="button" id="refreshButton" onclick="return Refresh();">Refresh</button>
-	<form action="speaker.php">
-		<input type="submit" value="Go to Speaker" />
-	</form>
-	<div style="display: inline-block;">
-		<h3>Accept Questions?</h3>
-	</div>
+<div class="submissionBlock">
+
+<div style="display: inline-block;">
+	<h3>Accept Questions?</h3>
+</div>
 	<input type="checkbox" id="questionAllow" name="questionsOnOff" value="Submit"/>
 		<form action="put.php" method="get">
 			<textarea readonly type="text" name="userName" id="uName">Username</textarea>
@@ -49,7 +56,7 @@
 			<input type="submit" name="delete" id="y" value="Delete Question!" onclick="if(confirm('Are you sure you want to delete this question?')) return true; return false;">
 		</form>
 
-	</div>
+</div>
 	<script type="text/javascript">
 	  $('#selections li').click(function() {
 		var question = $(this).text();
