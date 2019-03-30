3aa700b30fa01475f786602a99992b9b55fdc2ab
Aa-Whittaker <47148064+Aa-Whittaker@users.noreply.github.com>
2019-03-30 18:31:38 +0000

    Updates question sort by priority

--- speaker.php
+++ speaker.php
@@ -8,12 +8,23 @@
         <?php
             $jsondata = file_get_contents('https://cadgroup2.jdrcomputers.co.uk/api/questions');
             $json = json_decode($jsondata, true);
-            $output = '<ul id="selections">';
+			$questions = array();
             foreach($json as $question) {
-                $output .= "<li>".$question['userID'].": ".$question['question']."<div style='display: none;'>".'-'.$question['questionID']."</div></li>";
+                $line = $question['priority']."".$question['question'];
+				$line2 = substr($line,0 , 1);
+				if ($line2 != 0) {
+					array_push($questions, $line);
+				}
             }
-            $output .= "</ul>";
-            echo $output;
+			sort($questions);
+			echo '<ul>';
+			foreach($questions as $ques) {
+				$quest = "".$ques;
+				$q = substr($quest, 1);
+				echo '<li>'.$q.'</li>';
+			}
+			echo '</ul>';
+
         ?>
         </div>
         <!--/list-->
@@ -21,7 +32,7 @@
             <button type="button" onclick="return Refresh();">Refresh</button>
             <textarea style="font-size: 35px;" rows="10%" cols="40%" id="demo" readonly="true" oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'>Please select a question</textarea>
 			<form action="moderator.php">
-				<input type="submit" value="Go to Speaker" />
+				<input type="submit" value="Go to Moderator" />
 			</form>
         </div>
         <script type="text/javascript">
