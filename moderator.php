6945ffc3e02808a56ebe6899820318a07069067b
Aa-Whittaker <47148064+Aa-Whittaker@users.noreply.github.com>
2019-03-31 18:36:21 +0100

    AddedNavigationAddedEnableQuestions

--- moderator.php
+++ moderator.php
@@ -14,11 +14,12 @@
 	$nameJson = json_decode($nameJsonData, true);
 	//$roomTitle = json_decode(file_get_contents("https://cadgroup2.jdrcomputers.co.uk/api/sessions/{$sessionID}"), true)
 	echo '<h1 id="sessionName">'.$nameJson['sessionName'].'</h1>';
+	$allowQuest = $nameJson['acceptingQuestions'];
 ?>
 	<!--Refresh Button and speaker button-->
 <div id="optionsButtons" style="padding-left: 10px; padding-bottom: 30px;">
 	<div  id="refreshButton"  class="btn btn-success useButton" onclick="return Refresh();" style="padding: 7px;">Refresh</div>
-	<div  id="speakerButton"  class="btn btn-success useButton" onclick="window.location.href = 'speaker.php'" style="padding: 7px;">Go to Speaker</div>
+	<div  id="speakerButton"  class="btn btn-success useButton" onclick="window.location.href = 'speaker.php?sessionID=<?php echo "$sessionID"; ?>'" style="padding: 7px;">Go to Speaker</div>
 </div>
 <div class="list">
     <?php
@@ -37,13 +38,13 @@
 
 <div class="submissionBlock">
 
-<div style="display:inline;">
-	<h3>Accept Questions?</h3>
-	<div  id="refreshButton"  class="btn btn-success acceptButton" onclick="return Refresh();" style="padding: 7px;">Enable</div>
-</div>
+
 	<!--<input type="checkbox" id="questionAllow" name="questionsOnOff" value="Submit"/>-->
 	<form action="put.php" method="get">
-		
+		<div style="display:inline;">
+			<h3>Accept Questions?</h3>
+			<input type="submit" class="btn btn-success acceptButton" name="enable" value="Enable!"onclick="" style="padding: 7px;">
+		</div>
         <textarea type="text" name="updatedq" id="updateq" class="form-control" rows="10" cols="65" maxlength="250">On click questions will appear here... In this box...</textarea>
 		<!-- submit, delete and modify priority stuff -->
 		<h4>Set Question Priority</h4>
@@ -52,16 +53,17 @@
 			<input type="submit" class="btn btn-success" name="delete" id="deleteBtn" value="Delete Question!" onclick="if(confirm('Are you sure you want to delete this question?')) return true; return false;">
 			<div class="col-xs-2 col-ys-1">
 				<select name="priority" type="text" class="form-control" >
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
 					<option value="4">4</option>
+					<option value="3">3</option>
+					<option value="2">2</option>
+					<option value="1">1</option>
 				</select>
 			</div>
 		</div>
 		<!-- end of modifyOptions -->
 		<br>
 		<textarea readonly type="text" name="userName" id="uName" style="display: none">Username</textarea>
+		<textarea type="text" name="enableQuest" id="enableQues" style="display: none;"><?php echo "$allowQuest"; ?></textarea>
 		<textarea type="text" name="sessionID" id="questionID" style="display: none;"><?php echo "$sessionID"; ?></textarea>
 		<textarea type="text" name="user" id="question" rows="1" cols="5" style="display: none">question id will appear here</textarea>
 		
--- put.php
+++ put.php
@@ -42,4 +42,62 @@ curl_close($ch);
 header("Location: moderator.php?submission=success&sessionID={$sessionID}");
 exit();
 }
+
+
+if(isset($_GET["complete"])) {
+$question = $_GET['updatedq'];
+$priority = $_GET['priority'];
+$questionID = $_GET['user'];
+$sessionID = $_GET['sessionID'];
+$url = "https://cadgroup2.jdrcomputers.co.uk/api/questions/".$questionID;
+$data = array('question'=>$question,'priority'=>$priority);
+$data_json = json_encode($data);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response  = curl_exec($ch);
+curl_close($ch);
+header("Location: speaker.php?submission=success&sessionID={$sessionID}");
+exit();
+}
+
+if(isset($_GET["enable"])) {
+$sessionID = $_GET['sessionID'];
+$e = $_GET['enableQuest'];
+if ($e == 1) {
+	$url = "https://cadgroup2.jdrcomputers.co.uk/api/sessions/".$sessionID;
+	$data = array('acceptingQuestions'=>0);
+	$data_json = json_encode($data);
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+	curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$response  = curl_exec($ch);
+	curl_close($ch);
+	header("Location: moderator.php?submission=success&sessionID={$sessionID}");
+	exit();
+} else {
+	$url = "https://cadgroup2.jdrcomputers.co.uk/api/sessions/".$sessionID;
+	$data = array('acceptingQuestions'=>1);
+	$data_json = json_encode($data);
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+	curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$response  = curl_exec($ch);
+	curl_close($ch);
+	header("Location: moderator.php?submission=success&sessionID={$sessionID}");
+	exit();
+}
+}
 ?>
